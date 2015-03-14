794265795d800aea2b0a16ad4dd3f59d1b78996b
unknown <posgrad@FA7-L42M12.7setlabs.local>
2015-03-14 14:05:11 -0300

    ingredientes da receita

---
+++ WP Final Project/Page/IngredientesReceita.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using WP_Final_Project.Resources.db.model;
+using WP_Final_Project.View;
+using WP_Final_Project.Resources.db;
+
+namespace WP_Final_Project.Page
+{
+    public partial class IngredientesReceita: PhoneApplicationPage
+    {
+        public static IngredientesReceita instance;
+
+        public IngredientesReceita()
+        {
+            InitializeComponent();
+            instance = this;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            foreach (Ingrediente ingrediente in App.receitaSelecionada.Ingredientes) 
+            {
+                ItemIngredienteReceita itemIngredienteReceita = new ItemIngredienteReceita(ingrediente);
+                ListaDeIngredientes.Children.Add(itemIngredienteReceita);
+            }
+        }
+
+        public void verIngredienteSelecionado()
+        {
+            NavigationService.Navigate(new Uri("/Page/VerIngrediente.xaml", UriKind.Relative));
+        }
+
+    }
+}
---
+++ WP Final Project/View/ItemIngredienteReceita.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using WP_Final_Project.Resources.db.model;
+using WP_Final_Project.Page;
+
+namespace WP_Final_Project.View
+{
+    public partial class ItemIngredienteReceita : UserControl
+    {
+        public ItemIngredienteReceita(Ingrediente ingrediente)
+        {
+            InitializeComponent();
+            NomeIngrediente.Text = ingrediente.Produto.Nome;
+        }
+        
+        private void LayoutRoot_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            IngredientesReceita.instance.verIngredienteSelecionado();
+        }
+
+    }
+}
